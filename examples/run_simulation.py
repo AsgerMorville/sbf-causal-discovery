4de7172c22e5719c6e9523227902b5027e74ba1b
Asger <asgermorville@gmail.com>
2025-07-13 08:35:05 +0200

    add gitignore

--- examples/run_simulation.py
+++ examples/run_simulation.py
@@ -3,10 +3,14 @@ from graph_search.ordering_search import GreedyOrderingSearch
 from sbf_core.regression import SBF_fitter
 from graph_search.variable_selection import EdgePruner
 
+########################################################
+# 1. Simulation configuration and data generation
+########################################################
+
 # Set configuration for simulation
 n = 100 # Number of samples
 p = 10 # Number of observed variables
-lmbda = 0.5 # Controls the degree of sparsity of the estimated DAG. Higher lmbda gives estimates that are more sparse.
+lmbda = 0.5 # Controls the sparsity of the estimated DAG. Higher lmbda gives estimates that are sparser.
 length_scale = 0.4 # Controls the wigglyness of the structural equation functions
 hetero_var = False # Set to true to simulate heterogeneous variance
 dense_graph = 'never_dense' #never_dense: sparse setting. Set to 'always_dense' for dense setting.
@@ -14,9 +18,10 @@ dense_graph = 'never_dense' #never_dense: sparse setting. Set to 'always_dense' 
 # Generate DAG and samples
 synth_data = SyntheticData(p=p, n=n, length_scale=length_scale, hetero_var=hetero_var, dense_graph=dense_graph)
 
-############################
-# Fitting the model
-############################
+########################################################
+# 2. Fitting the fully connected DAG
+########################################################
+
 data = synth_data.X
 
 # Use the smooth backfitting method as the fitter
@@ -28,13 +33,20 @@ model = GreedyOrderingSearch(fitter, data)
 # Run the training phase.
 model.train()
 
+########################################################
+# 3. Pruning the DAG
+########################################################
+
 # Prune the graph estimate
 pruner = EdgePruner(G=model.G, X=data, lambda_par=lmbda, selection_method='fLassoMan')
 
 # Run the pruning process
 pruner.train()
 
-# Final estimate:
+########################################################
+# 4. Inspect result
+########################################################
+
 print("Graph estimate: ", pruner.G.G)
 print("True graph: ", synth_data.G.G)
 
